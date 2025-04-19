61e4988753cbb815c9e1a1ddfaf8db417e7a463f
karan-mishra-sudo <mishrazack69@gmail.com>
2025-04-19 20:08:46 +0530

    Delete src directory

--- src/Redux/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-
-import { combineReducers } from 'redux';
-import dataReducer from './DataReducers.js';
-
-const rootReducer = combineReducers({
-  Data: dataReducer,
-}); 
-
-export default rootReducer;
--- src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
-import rootReducer from "./Reducers"; // You can combine reducers here
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
